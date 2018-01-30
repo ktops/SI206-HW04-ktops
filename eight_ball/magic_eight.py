5c75e563898d876e5b09cd74c3d5e72e08e77446
Natalie <nataliecieslak@yahoo.com>
2018-01-30 18:24:00 -0500

    first ten answers

--- eight_ball/magic_eight.py
+++ eight_ball/magic_eight.py
@@ -1,3 +1,8 @@
 def user_question():
     user_input = input("What is your quesiton? ")
     return user_input
+
+possible_answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely",
+                    "You may rely on it", "As I see it, yes", "Most likely", "Outlook good","Yes", "Signs point to yes"]
+
+answer_to_question = random.choice(possible_answers)
