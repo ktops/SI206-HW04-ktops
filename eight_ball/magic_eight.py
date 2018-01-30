1866d3d0b406d1c5020030cf6676bac8b60b806f
Kelsey Toporski <ktops@umich.edu>
2018-01-30 18:36:47 -0500

    looping

--- eight_ball/magic_eight.py
+++ eight_ball/magic_eight.py
@@ -1,3 +1,12 @@
 def user_question():
     user_input = input("What is your quesiton? ")
     return user_input
+
+user_input = ""
+while user_input is not "quit":
+    user_input = user_question()
+
+    if user_input[-1] is not "?":
+        print("I'm sorry, I can only answer questions.")
+    else:
+        break
