c8e5df2c3cb12577bb5075075e8021157c145cc8
Kelsey Toporski <33010084+ktops@users.noreply.github.com>
2018-01-30 19:17:51 -0500

    Delete magic_eight.py

--- eight_ball/magic_eight.py
+++
@@ -1,9 +0,0 @@
-def user_question():
-    user_input = input("What is your quesiton? ")
-    return user_input
-
-import random
-questions_list=  ["Ask again later", "Better not to tell you now", "Cannot predict now",
-                "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good",
-                "Very doubtful"]
-answer_to_question = random.choice(questions_list)
